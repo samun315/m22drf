b5df08fcedc26a40a7ea48decbea1722fe8c0a92
Samun Chowdhury <samun315@gmail.com>
2023-09-02 22:43:01 +0600

    web route created

--- routes/frontend.php
+++ routes/frontend.php
@@ -13,3 +13,13 @@ Route::get('login', [AuthController::class, 'showLoginForm'])->name('frontend.sh
 Route::get('register', [AuthController::class, 'showRegisterForm'])->name('frontend.showRegisterForm');
 Route::get('reset-password', [AuthController::class, 'showResetPasswordForm'])->name('frontend.showResetPasswordForm');
 Route::get('change-password', [AuthController::class, 'showChangePasswordForm'])->name('frontend.showChangePasswordForm');
+
+
+//web pages routes
+Route::get('/about-us', [FrontendController::class, 'aboutUs'])->name('frontend.aboutUs');
+Route::get('/contact-us', [FrontendController::class, 'contactUs'])->name('frontend.contactUs');
+Route::get('/project', [FrontendController::class, 'project'])->name('frontend.project');
+Route::get('/event', [FrontendController::class, 'event'])->name('frontend.event');
+Route::get('/blog', [FrontendController::class, 'event'])->name('frontend.blog');
+
+
