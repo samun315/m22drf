d6dd471759eff532d40a0643e678313ed6eb44a9
ripon <riponahmed2201@gmail.com>
2023-09-03 18:07:05 +0600

    preloader close

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -11,16 +11,6 @@
 
     <div class="boxed_wrapper ltr">
 
-        <!-- Preloader -->
-        <div class="loader-wrap">
-            <div class="preloader">
-                <div class="preloader-close">Preloader Close</div>
-            </div>
-            <div class="layer layer-one"><span class="overlay"></span></div>
-            <div class="layer layer-two"><span class="overlay"></span></div>
-            <div class="layer layer-three"><span class="overlay"></span></div>
-        </div>
-
         <!-- page-direction -->
         <div class="page_direction">
             <div class="demo-rtl direction_switch"><button class="rtl">RTL</button></div>
