bc8cb4d955f0ea9a75a27d1de1a247a697b39512
ripon <riponahmed2201@gmail.com>
2024-02-28 22:42:38 +0600

    all delete btn added in blade file

--- resources/views/admin/volunteer/index.blade.php
+++ resources/views/admin/volunteer/index.blade.php
@@ -16,11 +16,11 @@
                                 <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
                                     <rect x="5" y="5" width="5" height="5" rx="1" fill="#000000"></rect>
                                     <rect x="14" y="5" width="5" height="5" rx="1" fill="#000000"
-                                        opacity="0.3"></rect>
+                                          opacity="0.3"></rect>
                                     <rect x="5" y="14" width="5" height="5" rx="1" fill="#000000"
-                                        opacity="0.3"></rect>
+                                          opacity="0.3"></rect>
                                     <rect x="14" y="14" width="5" height="5" rx="1" fill="#000000"
-                                        opacity="0.3"></rect>
+                                          opacity="0.3"></rect>
                                 </g>
                             </svg>
                             <span class="card-label fw-bolder fs-3 mb-1">Manage Volunteer</span>
@@ -30,11 +30,11 @@
                         <a href="{{ route('admin.volunteer.create') }}" class="btn btn-sm btn-light-success">
                             <span class="svg-icon svg-icon-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                    fill="none">
+                                     fill="none">
                                     <rect opacity="0.5" x="11.364" y="20.364" width="16" height="2" rx="1"
-                                        transform="rotate(-90 11.364 20.364)" fill="black" />
+                                          transform="rotate(-90 11.364 20.364)" fill="black"/>
                                     <rect x="4.36396" y="11.364" width="16" height="2" rx="1"
-                                        fill="black" />
+                                          fill="black"/>
                                 </svg>
                             </span>
                             Add New
@@ -53,73 +53,65 @@
                         <table class="table align-middle gs-0 gy-4">
                             <!--begin::Table head-->
                             <thead>
-                                <tr class="fw-bolder text-muted bg-light">
-                                    <th>SL</th>
-                                    <th>Image</th>
-                                    <th>Name</th>
-                                    <th>Designation</th>
-                                    <th>Email</th>
-                                    <th>Phone Number</th>
-                                    <th>Organization Name</th>
-                                    <th>About Your Self</th>
-                                    <th>Active</th>
-                                    <th>Action</th>
-                                </tr>
+                            <tr class="fw-bolder text-muted bg-light">
+                                <th>SL</th>
+                                <th>Image</th>
+                                <th>Name</th>
+                                <th>Designation</th>
+                                <th>Email</th>
+                                <th>Phone Number</th>
+                                <th>Organization Name</th>
+                                <th>About Your Self</th>
+                                <th>Active</th>
+                                <th>Action</th>
+                            </tr>
                             </thead>
                             <!--end::Table head-->
                             <!--begin::Table body-->
                             <tbody id="tbody">
 
-                                @foreach ($results as $value)
-                                    <tr>
-                                        <td> {{ $loop->iteration }} </td>
-                                        <td>
-                                            <img style="width: 80px; height: 80px; border-radius: 50%"
-                                                src="{{ asset('uploads/volunteer/' . $value->image) }}"
-                                                alt="{{ $value->title }}">
-                                        </td>
-                                        <td> {{ $value->name }} </td>
-                                        <td> {{ $value->designation }} </td>
-                                        <td> {{ $value->email }} </td>
-                                        <td> {{ $value->phone_number }} </td>
-                                        <td> {{ $value->organization_name }} </td>
-                                        <td> {!! $value->about_your_self !!} </td>
-                                        <td>
-                                            <label class="form-check form-switch form-check-custom form-check-solid">
+                            @foreach ($results as $value)
+                                <tr>
+                                    <td> {{ $loop->iteration }} </td>
+                                    <td>
+                                        <img style="width: 80px; height: 80px; border-radius: 50%"
+                                             src="{{ asset('uploads/volunteer/' . $value->image) }}"
+                                             alt="{{ $value->title }}">
+                                    </td>
+                                    <td> {{ $value->name }} </td>
+                                    <td> {{ $value->designation }} </td>
+                                    <td> {{ $value->email }} </td>
+                                    <td> {{ $value->phone_number }} </td>
+                                    <td> {{ $value->organization_name }} </td>
+                                    <td> {!! $value->about_your_self !!} </td>
+                                    <td>
+                                        <label class="form-check form-switch form-check-custom form-check-solid">
 
-                                                @if ($value->status == 'YES')
-                                                    <input class="form-check-input"
-                                                        onchange="updateStatus('NO', {{ $value->id }})" name="status"
-                                                        type="checkbox" value="YES" checked="checked" />
-                                                @else
-                                                    <input class="form-check-input"
-                                                        onchange="updateStatus('YES', {{ $value->id }})" name="status"
-                                                        type="checkbox" value="NO" />
-                                                @endif
+                                            @if ($value->status == 'YES')
+                                                <input class="form-check-input"
+                                                       onchange="updateStatus('NO', {{ $value->id }})" name="status"
+                                                       type="checkbox" value="YES" checked="checked"/>
+                                            @else
+                                                <input class="form-check-input"
+                                                       onchange="updateStatus('YES', {{ $value->id }})" name="status"
+                                                       type="checkbox" value="NO"/>
+                                            @endif
 
-                                                <span class="form-check-label fw-bold text-muted">
+                                            <span class="form-check-label fw-bold text-muted">
                                                     {{ $value->status }}
                                                 </span>
-                                            </label>
-                                        </td>
-                                        <td>
+                                        </label>
+                                    </td>
+                                    <td>
+                                        <div class="btn-group">
                                             <a href="{{ route('admin.volunteer.edit', $value->id) }}"
-                                                class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1">
-                                                <span class="svg-icon svg-icon-3">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                        viewBox="0 0 24 24" fill="none">
-                                                        <path opacity="0.3"
-                                                            d="M21.4 8.35303L19.241 10.511L13.485 4.755L15.643 2.59595C16.0248 2.21423 16.5426 1.99988 17.0825 1.99988C17.6224 1.99988 18.1402 2.21423 18.522 2.59595L21.4 5.474C21.7817 5.85581 21.9962 6.37355 21.9962 6.91345C21.9962 7.45335 21.7817 7.97122 21.4 8.35303ZM3.68699 21.932L9.88699 19.865L4.13099 14.109L2.06399 20.309C1.98815 20.5354 1.97703 20.7787 2.03189 21.0111C2.08674 21.2436 2.2054 21.4561 2.37449 21.6248C2.54359 21.7934 2.75641 21.9115 2.989 21.9658C3.22158 22.0201 3.4647 22.0084 3.69099 21.932H3.68699Z"
-                                                            fill="black" />
-                                                        <path
-                                                            d="M5.574 21.3L3.692 21.928C3.46591 22.0032 3.22334 22.0141 2.99144 21.9594C2.75954 21.9046 2.54744 21.7864 2.3789 21.6179C2.21036 21.4495 2.09202 21.2375 2.03711 21.0056C1.9822 20.7737 1.99289 20.5312 2.06799 20.3051L2.696 18.422L5.574 21.3ZM4.13499 14.105L9.891 19.861L19.245 10.507L13.489 4.75098L4.13499 14.105Z"
-                                                            fill="black" />
-                                                    </svg>
-                                                </span>
+                                               class="btn btn-primary btn-sm">
                                             </a>
-                                        </td>
-                                    </tr>
-                                @endforeach
+                                            <a href="" class="btn btn-danger btn-sm">Delete</a>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
 
                             </tbody>
                             <!--end::Table body-->
@@ -154,7 +146,7 @@
                     _token: v_token
                 },
                 dataType: 'json',
-                success: function(res) {
+                success: function (res) {
 
                     Swal.fire({
                         position: 'top-end',
